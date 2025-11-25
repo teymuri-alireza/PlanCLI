e6d50f16622c0c8364a47c2beee3ea11394bb5c0
teymuri-alireza <kishijung@gmail.com>
2025-11-25 20:31:58 +0330

    chore: init database models

---
+++ TaskCLI/Models/DatabaseController.cs
@@ -0,0 +1,36 @@
+using System.Text.Json;
+
+namespace TaskCLI.Models;
+
+public class DatabaseController
+{
+    private readonly string _filePath;
+    public List<TodoItem> Items { get; set; } = new();
+    public DatabaseController(string filePath)
+    {
+        _filePath = filePath;
+        Load();
+    }
+
+    public void Load()
+    {
+        if (!File.Exists(_filePath))
+        {
+            Items = new List<TodoItem>();
+            Save();
+            return;
+        }
+        var json = File.ReadAllText(_filePath);
+        Items = JsonSerializer.Deserialize<List<TodoItem>>(json) ?? new();
+    }
+
+    public void Save()
+    {
+        var json = JsonSerializer.Serialize(Items, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+
+        File.WriteAllText(_filePath, json);
+    }
+}
---
+++ TaskCLI/Models/TodoItem.cs
@@ -0,0 +1,7 @@
+namespace TaskCLI.Models;
+
+public class TodoItem
+{
+    public string? Title { get; set; }
+    public string? Description { get; set; } = "";
+}
