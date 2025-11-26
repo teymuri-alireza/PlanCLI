dfd7c0e0963c16ddd4aae8306bcf076a444a9886
teymuri-alireza <kishijung@gmail.com>
2025-11-26 19:55:35 +0330

    feat: add checkbox to TodoItem Model

--- TaskCLI/Models/TodoItem.cs
+++ TaskCLI/Models/TodoItem.cs
@@ -3,5 +3,6 @@ namespace TaskCLI.Models;
 public class TodoItem
 {
     public string? Title { get; set; }
-    public string? Description { get; set; } = "";
+    public string? Description { get; set; }
+    public bool IsDone { get; set; } = false;
 }
